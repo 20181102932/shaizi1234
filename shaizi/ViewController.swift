477849f52b1e8c851b17310d7d9a4c74fe64ef7e
20181102932 <908907990@qq.com>
2019-10-16 11:32:28 +0800

    Initial Commit

---
+++ shaizi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  shaizi
+//
+//  Created by s20181102932 on 2019/10/16.
+//  Copyright © 2019 wjx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
